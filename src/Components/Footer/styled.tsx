cadef71638adbb598f9133f6c015f86439324e7d
Thịnh Trần <thinhtv@uniorbit.org>
2025-02-24 14:37:49 +0700

    feat: update footer

--- src/Components/Footer/styled.tsx
+++ src/Components/Footer/styled.tsx
@@ -2,4 +2,8 @@ import styled from "styled-components";
 
 export const FooterContainer = styled.footer`
   padding-top: 200px;
+  @media screen and (max-width: 767.98px) {
+    padding-left: 20px;
+    padding-right: 20px;
+  }
 `;
